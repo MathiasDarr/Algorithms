e05b7e42e42eb548de92e1aef67353db4f43746d
mddarr <mddarr@gmail.com>
2020-10-12 11:26:19 -0700

    Implment Kadane's algorithm in Python, Java, C++ & Scala

---
+++ dakobed-algorithms/cpp-algorithms/arrays/Solution.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mddarr on 10/12/20.
+//
+
+#include "Solution.h"
---
+++ dakobed-algorithms/cpp-algorithms/arrays/Solution.h
@@ -0,0 +1,16 @@
+//
+// Created by mddarr on 10/12/20.
+//
+
+#ifndef CPP_ALGORITHMS_SOLUTION_H
+#define CPP_ALGORITHMS_SOLUTION_H
+
+
+
+class Solution {
+
+};
+
+
+
+#endif //CPP_ALGORITHMS_SOLUTION_H
